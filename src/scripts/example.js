2587972251e3c2a1d88c9f407139d9e4bc95ada7
zahiahmed <zahi.ahmed33@gmail.com>
2023-01-13 12:33:25 -0500

    tutortial setup test

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,14 +1,14 @@
 class Example{
     constructor(ele) {
         this.ele = ele;
-        this.ele.innerHTML = "<h1> It's Alive!!! </h1>"
+        this.ele.innerHTML = "<h1> TFT-Aa </h1>"
         this.handleClick = this.handleClick.bind(this);
         this.ele.addEventListener('click', this.handleClick);
     }
 
     handleClick(e) {
         e.preventDefault();
-        this.ele.children[0].innerText = 'Ouch!'
+        this.ele.children[0].innerText = '...in develpoment'
     }
 }
 
