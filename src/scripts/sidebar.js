8c3ed2696bd257cb1f42c20b990195d246e60fe0
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-01-14 19:50:28 -0500

    searchbar works

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -3,22 +3,35 @@ import Icon from './icon';
 
 class Sidebar{
     constructor(units){
+        this.units = units;
+        this.ele = document.querySelector("#champ_list")
+        this.listIcons();
+        this.search = document.getElementById("searchChamp");
+        this.handleSearch = this.handleSearch.bind(this);
+        this.search.addEventListener("keyup", this.handleSearch)
+    }
+
+    handleSearch(e){
+        e.preventDefault();
+        this.listIcons(this.search.value.toLowerCase());
+    }
+
+    sort(){
+    }
+
+    listIcons(str=""){
         const invalidUnits = ["Volcanic Sol", "Giant Crabgot", "Hackerim", "Mutant Zac"];
         this.champs = [];
-        this.ele = document.querySelector("#sidebar");
-        for(let i = 0; i<units.length; i++) {
-            const champ = new Unit(units[i]);
-            // console.log(champ);
-            if(!invalidUnits.includes(champ.name)){
+        this.ele.innerHTML = "";
+        for(let i = 0; i<this.units.length; i++) {
+            const champ = new Unit(this.units[i]);
+            if(!invalidUnits.includes(champ.name) && (champ.name.toLowerCase().includes(str))){
                 this.champs.push(champ);
                 const unitIcon = new Icon(champ, 50, 50);
                 this.ele.append(unitIcon.ele);
             }
+        }
 
-        }        
-        console.log(this.champs);
-    }
-    sort(){ //override sort algorithm
     }
 }
 
